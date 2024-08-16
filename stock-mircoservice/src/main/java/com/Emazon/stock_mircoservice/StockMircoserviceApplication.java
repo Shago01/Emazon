34c922fbfae205b269a8d355d6532039fea550eb
Shago01 <kgsanty2001@gmail.com>
2024-08-16 14:27:54 -0500

    Starting stock microservice project

---
+++ stock-mircoservice/src/main/java/com/Emazon/stock_mircoservice/StockMircoserviceApplication.java
@@ -0,0 +1,13 @@
+package com.Emazon.stock_mircoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMircoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMircoserviceApplication.class, args);
+	}
+
+}
---
+++ stock-mircoservice/src/test/java/com/Emazon/stock_mircoservice/StockMircoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Emazon.stock_mircoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMircoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
